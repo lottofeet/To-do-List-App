9b8be3c08d4bfcdfb313cd65763b988e9e20ebfc
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-06-08 13:46:34 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function newItem(){
+function newItem() {
   let li = $('<li></li>');
   let inputValue = $('#input').val();
   li.append(inputValue);
@@ -9,18 +9,18 @@ function newItem(){
     // create new to do item
     let list = $('#list');
     list.append(li);
-}
-  
+    }
+
 
  //2. Crossing out an item from the list of items:
    function crossOut() {
     li.toggleClass('strike');
   }
 
-  li.on("dblclick", crossOut);
+  li.on('dblclick', crossOut);
 
  //3(i). Adding the delete button "X": 
-   let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+  let crossOutButton = $('<crossOutButton>X</crossOutButton>');
   li.append(crossOutButton);
 
   crossOutButton.on('click', deleteListItem);
