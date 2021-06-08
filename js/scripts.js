979a1a4b7f3746cd1a1d65864ca5c14a88782dc4
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-06-08 13:28:43 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 function newItem(){
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-
-// //1. Adding a new item to the list of items: 
-   let li = $('<li></li>');
-   let inputValue = $('#input').val();
-   li.append(inputValue);
-
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = $('#list');
-     list.append(li);
-   }
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    // create new to do item
+    let list = $('#list');
+    list.append(li);
+}
   
 
  //2. Crossing out an item from the list of items:
@@ -33,4 +31,4 @@ function newItem(){
  // 4. Reordering the items: 
    $('#list').sortable();
 
-}
+};
